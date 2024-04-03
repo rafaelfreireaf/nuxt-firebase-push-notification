5eb56081e2d5d5596b64b436af4f15808241ba72
Rafael Freire <rafael.freire@arenafantasy.com>
2024-04-03 13:55:40 -0300

    First Commit

---
+++ plugins/firebase.client.ts
@@ -0,0 +1,41 @@
+import { initializeApp, getApps } from 'firebase/app'
+import { getMessaging, onMessage, type Messaging } from 'firebase/messaging'
+
+declare module '#app' {
+  interface NuxtApp {
+    $messaging: Messaging
+  }
+}
+
+declare module 'vue' {
+  interface ComponentCustomProperties {
+    $messaging: Messaging
+  }
+}
+
+export default defineNuxtPlugin(() => {
+  const app = getApps()[0] ?? initializeApp({
+    apiKey: "AIzaSyBa0nUo74uA2w6EMf5Y_iKzBiLwQZUdnJw",
+    authDomain: "vue-notification-4a729.firebaseapp.com",
+    projectId: "vue-notification-4a729",
+    storageBucket: "vue-notification-4a729.appspot.com",
+    messagingSenderId: "372119211975",
+    appId: "1:372119211975:web:ed232e2519b9e99fd953e2",
+    measurementId: "G-NFHCEVQGY5"
+  })
+  
+  const messaging = getMessaging(app)
+  
+  // This runs whenever a message is received:
+  //   - When the page is open
+  //   - When the user clicked the bg notification
+  onMessage(messaging, (payload) => {
+    alert(JSON.stringify(payload, null, 2))
+  })
+  
+  return {
+    provide: {
+      messaging,
+    }
+  }
+});
---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,34 @@
+// Import the firebase app / messaging packages
+importScripts('https://www.gstatic.com/firebasejs/10.10.0/firebase-app-compat.js')
+importScripts('https://www.gstatic.com/firebasejs/10.10.0/firebase-messaging-compat.js')
+
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+const app = firebase.initializeApp({
+  apiKey: "AIzaSyBa0nUo74uA2w6EMf5Y_iKzBiLwQZUdnJw",
+  authDomain: "vue-notification-4a729.firebaseapp.com",
+  projectId: "vue-notification-4a729",
+  storageBucket: "vue-notification-4a729.appspot.com",
+  messagingSenderId: "372119211975",
+  appId: "1:372119211975:web:ed232e2519b9e99fd953e2",
+  measurementId: "G-NFHCEVQGY5"
+});
+
+// Initialize messaging
+const messaging = firebase.messaging()
+
+// Listen to bg messages
+messaging.onBackgroundMessage(payload => {
+  console.log("Received a bg message: ", payload);
+
+
+  const title = payload.notification.title
+  const notification = {
+    body: "Notification Body",
+    icon: "/icon.png"
+  }
+
+  // Show notification when message received
+  self.registration.showNotification(title, notification);
+})
